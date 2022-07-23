8ea843d2d62da899bc571d002b77f1fbb54b7d93
Brandon Lin <bwlin93@gmail.com>
2022-07-23 18:03:02 -0500

    changes db.js to use sequelize instead of PG.pool

--- service/db.js
+++ service/db.js
@@ -1,11 +1,45 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "brandon",
-  password: "",
-  database: "todo_database",
+// const pool = new Pool({
+//   user: "brandon",
+//   password: "",
+//   database: "todo_database",
+//   host: "localhost",
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
+const os = require("os");
+
+const Sequelize = require("sequelize");
+
+//  sequelize = new Sequelize("<database>", "<username>", "<password>", {
+const sequelize = new Sequelize("todo_database", "brandon", "", {
   host: "localhost",
+  dialect: "postgres",
+  logging: null,
   port: 5432,
+  pool: {
+    max: parseInt(120 / os.cpus().length),
+    min: 2,
+    idle: 10000,
+    acquire: 20000,
+  },
+  retry: {
+    match:
+      "SequelizeDatabaseError: could not serialize access due to concurrent update",
+    max: 3,
+  },
 });
 
-module.exports = pool;
+// Test connection
+sequelize
+  .authenticate()
+  .then(() => console.log("Database is connected"))
+  .catch((error) => console.log("Error: " + error));
+
+module.exports = {
+  Sequelize,
+  sequelize,
+};

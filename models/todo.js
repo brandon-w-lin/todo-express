c1b236e21f3256d56aad0749c6833f582043003e
Brandon Lin <bwlin93@gmail.com>
2022-07-24 13:16:27 -0500

    creates todo model

---
+++ models/todo.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const db = require("../service/db"); // instance of Sequelize
+const { DataTypes } = require("sequelize");
+
+// module.exports = function (sequelize, DataTypes) {
+//   var Todo = db.define(
+//     "Todo",
+//     {
+//       todo_id: { type: Sequelize.INTEGER },
+//       description: { type: Sequelize.TEXT },
+//     }
+//     // {
+//     //   createdAt: "created_at",
+//     //   updatedAt: "updated_at",
+//     //   deletedAt: "deleted_at",
+//     //   paranoid: true,
+//     //   underscored: true,
+//     //   tableName: "creator",
+//     // }
+//   );
+
+//   return Todo;
+// };
+
+const Todo = db.define("todo", {
+  id: { type: DataTypes.INTEGER, primaryKey: true },
+  completed: { type: DataTypes.INTEGER },
+  description: { type: DataTypes.STRING },
+});
+
+module.exports = Todo;

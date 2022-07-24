a72b8a5e61955351cebb7a194953122893dd0ced
Brandon Lin <bwlin93@gmail.com>
2022-07-24 13:17:09 -0500

    updates routes to use sequelize instead of writing direct queries

--- routes/todos.js
+++ routes/todos.js
@@ -1,76 +1,94 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../service/db");
+const db = require("../service/db");
+const Todo = require("../models/todo");
 
 // INDEX
-router.get("/", async (req, res) => {
-  try {
-    const todos = await pool.query("SELECT * FROM todo");
-    res.json(todos.rows);
-  } catch (err) {
-    console.log(err.message);
-  }
+router.get("/", (req, res) => {
+  // res.send("hello");
+  Todo.findAll({ attributes: ["id", "completed", "description"] })
+    .then((todos) => {
+      console.log("received todos");
+
+      res.status(200).send(todos);
+    })
+    .catch((err) => {
+      console.log("error");
+      res.send(err);
+    });
 });
 
-// SHOW
-router.get("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
-      id,
-    ]);
-    if (todo.rows.length === 0) {
-      res.status(404).send("No todo with that ID found");
-    } else {
-      res.json(todo.rows);
-    }
-  } catch (err) {
-    console.log(err.message);
-  }
-});
+// const pool = require("../service/db");
 
-// CREATE
-router.post("/", async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
-      [description]
-    );
-    res.json(newTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+// // INDEX
+// router.get("/", async (req, res) => {
+//   try {
+//     const todos = await pool.query("SELECT * FROM todo");
+//     res.json(todos.rows);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// });
 
-// UPDATE
-router.put("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
+// // SHOW
+// router.get("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+//       id,
+//     ]);
+//     if (todo.rows.length === 0) {
+//       res.status(404).send("No todo with that ID found");
+//     } else {
+//       res.json(todo.rows);
+//     }
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// });
 
-    const updTodo = await pool.query(
-      "UPDATE todo SET description = $1 WHERE todo_id = $2",
-      [description, id]
-    );
-    res.json(updTodo);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+// // CREATE
+// router.post("/", async (req, res) => {
+//   try {
+//     const { description } = req.body;
+//     const newTodo = await pool.query(
+//       "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+//       [description]
+//     );
+//     res.json(newTodo.rows[0]);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
 
-// DELETE
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
+// // UPDATE
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const { description } = req.body;
 
-    const updTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
-      id,
-    ]);
-    res.json("todo was destroyed");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
+//     const updTodo = await pool.query(
+//       "UPDATE todo SET description = $1 WHERE todo_id = $2",
+//       [description, id]
+//     );
+//     res.json(updTodo);
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
+
+// // DELETE
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+
+//     const updTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+//       id,
+//     ]);
+//     res.json("todo was destroyed");
+//   } catch (err) {
+//     console.error(err.message);
+//   }
+// });
 
 module.exports = router;

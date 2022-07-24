38165f902815881c6fed566be1625f6e4b2cc82e
Brandon Lin <bwlin93@gmail.com>
2022-07-24 14:58:20 -0500

    reorganizes code, no change to content

--- models/todo.js
+++ models/todo.js
@@ -3,6 +3,23 @@
 const db = require("../service/db"); // instance of Sequelize
 const { DataTypes } = require("sequelize");
 
+const Todo = db.define(
+  "todo",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    completed: { type: DataTypes.INTEGER },
+    description: { type: DataTypes.STRING },
+  },
+  {
+    createdAt: "createdat",
+    updatedAt: "updatedat",
+    underscored: true,
+    // tableName: "creator",
+  }
+);
+
+module.exports = Todo;
+
 // module.exports = function (sequelize, DataTypes) {
 //   var Todo = db.define(
 //     "Todo",
@@ -22,20 +39,3 @@ const { DataTypes } = require("sequelize");
 
 //   return Todo;
 // };
-
-const Todo = db.define(
-  "todo",
-  {
-    // id: { type: DataTypes.INTEGER, primaryKey: true },
-    completed: { type: DataTypes.INTEGER },
-    description: { type: DataTypes.STRING },
-  },
-  {
-    createdAt: "createdat",
-    updatedAt: "updatedat",
-    underscored: true,
-    // tableName: "creator",
-  }
-);
-
-module.exports = Todo;

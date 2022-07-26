d5b1ab568a0ea6cf79b6fe2016a914b3e7fc4c38
Brandon Lin <bwlin93@gmail.com>
2022-07-26 10:29:35 -0500

    adds validations for username and email: unique, email is email; removes id from model (set by sequelize)

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define("users", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-    },
+    // id: {
+    //   type: DataTypes.INTEGER,
+    //   primaryKey: true,
+    // },
     username: {
       type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: "That username is already in use.",
+      },
     },
     email: {
       type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+      unique: {
+        args: true,
+        msg: "An account is already associated with that email address.",
+      },
     },
     password: {
       type: DataTypes.STRING,

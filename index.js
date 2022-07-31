6f9ca2b783ff663e973860fa2624c9aca733aa6f
Brandon Lin <bwlin93@gmail.com>
2022-07-31 13:02:50 -0500

    removes axios, installs cors

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-
-const db = require("./models"); // I think this is needed to initialize the global.db
-const Role = global.db.Role;
 app.use(express.json());
 
 // view engine setup
-const path = require("path");
+// const path = require("path");
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "jade");
 
@@ -14,17 +11,12 @@ const path = require("path");
 // For development, use force:true.
 // For production, will want to use .sync() without any parameters so as to avoid dropping data
 
-var sequelize_fixtures = require("sequelize-fixtures");
-
-const newUser = require("./controllers/users");
-
-const bcrypt = require("bcrypt");
-const axios = require("axios");
-
 global.db.sequelize.sync();
+// const bcrypt = require("bcrypt");
 // global.db.sequelize.sync({ force: true }).then(async () => {
-//   console.log("Drop database and resync");
+//     console.log("Drop database and resync");
 
+//   var sequelize_fixtures = require("sequelize-fixtures");
 //   await sequelize_fixtures.loadFile("./seeders/roles.json", global.db);
 //   const pw1 = await bcrypt.hash("password", 10);
 //   global.db.User.create({
@@ -46,15 +38,6 @@ global.db.sequelize.sync();
 //     password: pw3,
 //   });
 //   await sequelize_fixtures.loadFile("./seeders/todos.json", global.db);
-
-//   const my_jwt = await axios
-//     .post("http://localhost:3000/login", {
-//       username: "brandon",
-//       password: "password",
-//     })
-//     .then((response) => {
-//       global.my_jwt = response.data.accessToken;
-//     });
 // });
 
 // Routers

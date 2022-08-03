9e2078c5615dff073b05a36a833d75e3ed69fac1
Brandon Lin <bwlin93@gmail.com>
2022-08-03 10:34:32 -0500

    adds authority minimums for viewing roles

--- controllers/roles.js
+++ controllers/roles.js
@@ -1,88 +1,46 @@
 const User = global.db.User;
 const Role = global.db.Role;
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-require("dotenv").config();
 
-// const addRoleToUser = (userId, RoleId) => {
-//   User.findOne({ where: { userId: userId } })
-//     .then((user) => {
-//       console.log(user);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
-
-// const index = (req, res) => {
-//   User.findAll()
-//     .then((users) => {
-//       res.status(200).send(users);
-//     })
-//     .catch((err) => {
-//       res.send(err);
-//     });
-// };
-
-// SHOW
+// INDEX
 const index = async (req, res) => {
-  const roles = await Role.findAll({
-    include: [
-      {
-        model: User,
-        as: "users",
-        attributes: ["id", "username"],
-        through: { attributes: [] },
-      },
-    ],
-  });
-  res.send(roles);
+  if (req.highest_role < 2) {
+    res
+      .status(403)
+      .send("Unauthorized - you do not have permission to view this resource.");
+  } else {
+    const roles = await Role.findAll({
+      include: [
+        {
+          model: User,
+          as: "users",
+          attributes: ["id", "username"],
+          through: { attributes: [] },
+        },
+      ],
+    });
+    res.send(roles);
+  }
 };
 
 // SHOW
 const show = async (req, res) => {
-  const role = await Role.findByPk(1, {
-    include: [
-      {
-        model: User,
-        as: "users",
-        attributes: ["id", "username"],
-        through: { attributes: [] },
-      },
-    ],
-  });
-  res.send(role);
+  if (req.highest_role < 2) {
+    res
+      .status(403)
+      .send("Unauthorized - you do not have permission to view this resource.");
+  } else {
+    const role = await Role.findByPk(1, {
+      include: [
+        {
+          model: User,
+          as: "users",
+          attributes: ["id", "username"],
+          through: { attributes: [] },
+        },
+      ],
+    });
+    res.send(role);
+  }
 };
-
-// // CREATE
-// const create = async (req, res) => {
-//   const username = req.body.username;
-//   const email = req.body.email;
-//   const password = req.body.password;
-//   const password_confirmation = req.body.password_confirmation;
-//   if (password !== password_confirmation) {
-//     // this should be validated on the front end before sending request, but validating here as well.
-//     res
-//       .status(400)
-//       .send("Error: password and password_confirmation do not match.");
-//   } else {
-//     try {
-//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//       User.create({
-//         username: username,
-//         email: email,
-//         password: hashedPassword,
-//       })
-//         .then((response) => {
-//           res.status(201).send(response);
-//         })
-//         .catch((err) => {
-//           res.send(err);
-//         });
-//     } catch {
-//       res.status(500);
-//     }
-//   }
-// };
 
 module.exports = { index, show };
--- routes/roles.js
+++ routes/roles.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const RolesController = require("../controllers/roles");
+const authJWT = require("../middleware/authJWT");
+
 // INDEX
-router.get("/", RolesController.index);
+router.get("/", authJWT, RolesController.index);
 
 // // SHOW
-router.get("/:id", RolesController.show);
+router.get("/:id", authJWT, RolesController.show);
 
 // // CREATE
 // router.post("/", RolesController.create);

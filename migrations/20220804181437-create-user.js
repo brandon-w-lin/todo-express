7c3013bcbcc3b4977400ddeeddcc62f1133f9b58
Brandon Lin <bwlin93@gmail.com>
2022-08-04 13:21:07 -0500

    updates user model to use sequelize CLI standards

---
+++ migrations/20220804181437-create-user.js
@@ -0,0 +1,46 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "That username is already in use.",
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+        unique: {
+          args: true,
+          msg: "An account is already associated with that email address.",
+        },
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
--- models/user.js
+++ models/user.js
@@ -1,31 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    // id: {
-    //   type: DataTypes.INTEGER,
-    //   primaryKey: true,
-    // },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: {
-        args: true,
-        msg: "That username is already in use.",
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          args: true,
+          msg: "That username is already in use.",
+        },
       },
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isEmail: true,
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+        unique: {
+          args: true,
+          msg: "An account is already associated with that email address.",
+        },
       },
-      unique: {
-        args: true,
-        msg: "An account is already associated with that email address.",
-      },
+      password: DataTypes.STRING,
     },
-    password: {
-      type: DataTypes.STRING,
-    },
-  });
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
   return User;
 };

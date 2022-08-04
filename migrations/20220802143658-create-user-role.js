0857c6aa7149d26bf5037b21649c22adc56c2a26
Brandon Lin <bwlin93@gmail.com>
2022-08-04 13:19:59 -0500

    updates table name to be lowercase

--- migrations/20220802143658-create-user-role.js
+++ migrations/20220802143658-create-user-role.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict";
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('User_Roles', {
+    await queryInterface.createTable("user_roles", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       role_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('User_Roles');
-  }
+    await queryInterface.dropTable("user_roles");
+  },
 };
